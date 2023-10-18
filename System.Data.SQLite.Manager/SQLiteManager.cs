41c17e26c711360aef1cdca1904a8d042defb23d
Vas Grekousis <vasilios.grekousis@hotmail.com>
2023-10-18 20:49:37 +0300

    Add project files.

---
+++ System.Data.SQLite.Manager/SQLiteManager.cs
@@ -0,0 +1,143 @@
+﻿using System.Data.SQLite;
+using System;
+using System.Data;
+using System.Text;
+using System.Xml.Linq;
+
+namespace System.Data.SQLite.Manager
+{
+	public class SQLiteManager
+	{
+		private string connectionString;
+
+		public SQLiteManager(string dbFilePath, string version = "3")
+		{
+			connectionString = $"Data Source={dbFilePath}Version={version};";
+			SQLiteFunction.RegisterFunction(typeof(ConcatenateFunction)); // Register your custom SQLite function
+		}
+
+		public bool CreateDatabaseFile()
+		{
+			try
+			{
+				using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+				{
+					connection.Open();
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"SQLite Error: {ex.Message}");
+				return false;
+			}
+		}
+
+		public bool ExecuteNonQuery(string query)
+		{
+			try
+			{
+				using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+				{
+					connection.Open();
+					using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+					{
+						cmd.ExecuteNonQuery();
+					}
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"SQLite Error: {ex.Message}");
+				return false;
+			}
+		}
+
+		public DataTable ExecuteQuery(string query)
+		{
+			try
+			{
+				using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+				{
+					connection.Open();
+					using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+					{
+						using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+						{
+							DataTable dt = new DataTable();
+							adapter.Fill(dt);
+							return dt;
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"SQLite Error: {ex.Message}");
+				return null;
+			}
+		}
+
+		public DataTable Select(string tableName, string columns, string condition = null, string order = null)
+		{
+			string selectQuery = $"SELECT {columns} FROM {tableName}";
+
+			if (!string.IsNullOrEmpty(condition))
+			{
+				selectQuery += $" WHERE {condition}";
+			}
+
+			if (!string.IsNullOrEmpty(order))
+			{
+				selectQuery += $" ORDER BY {order}";
+			}
+
+			return ExecuteQuery(selectQuery);
+		}
+
+		public bool CreateTable(string tableName, string columns)
+		{
+			string createTableQuery = $"CREATE TABLE IF NOT EXISTS {tableName} ({columns});";
+			return ExecuteNonQuery(createTableQuery);
+		}
+
+		public bool Insert(string tableName, string columns, string values)
+		{
+			string insertQuery = $"INSERT INTO {tableName} ({columns}) VALUES ({values});";
+			return ExecuteNonQuery(insertQuery);
+		}
+
+		public bool Update(string tableName, string set, string condition)
+		{
+			string updateQuery = $"UPDATE {tableName} SET {set} WHERE {condition};";
+			return ExecuteNonQuery(updateQuery);
+		}
+
+		public bool Delete(string tableName, string condition)
+		{
+			string deleteQuery = $"DELETE FROM {tableName} WHERE {condition};";
+			return ExecuteNonQuery(deleteQuery);
+		}
+	}
+
+	// Custom Concatenate Function
+	[SQLiteFunction(Name = "concat", FuncType = FunctionType.Scalar, Arguments = 2)]
+	public class ConcatenateFunction : SQLiteFunction
+	{
+		public override object Invoke(object[] args)
+		{
+			if (args == null || args.Length == 0)
+				return null;
+
+			StringBuilder result = new StringBuilder();
+			foreach (object arg in args)
+			{
+				if (arg != null)
+					result.Append(arg == null ? string.Empty : arg);
+			}
+
+			return result.ToString();
+		}
+	}
+}
